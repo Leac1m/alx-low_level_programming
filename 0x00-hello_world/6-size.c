8a73cd030922336feab580b65f7480c64b7a2597
Leac1m <obemichael10@gmail.com>
2022-12-08 17:37:50 -0800

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-
+/*
+A script to check storage size of varables
+*/	
 int main(void)
 {
 	char a;
@@ -9,7 +11,7 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %d bytes(s)\nSize of a int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of long long int: %d byte(s)\nSize of a float: %d byte(s)\n", (unsigned long)sizeof(a),(unsigned long)sizeof(b),(unsigned long)sizeof(c),(unsigned long)sizeof(d),(unsigned long)sizeof(e));
+	printf("Size of a char: %d byte(s)\nSize of a int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of long long int: %d byte(s)\nSize of a float: %d byte(s)\n" , (unsigned long)sizeof(a) ,(unsigned long)sizeof(b) ,(unsigned long)sizeof(c) ,(unsigned long)sizeof(d) ,(unsigned long)sizeof(e));
 
 	return (0);
 
