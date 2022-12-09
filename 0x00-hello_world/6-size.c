552f9ac905169f76a8694ed90bda11985e697f6f
Leac1m <obemichael10@gmail.com>
2022-12-08 19:07:08 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a int: %d byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of long long int: %d byte(s)\n" (unsigned long)sizeof(d));
+	printf("Size of a long long int: %d byte(s)\n" (unsigned long)sizeof(d));
 	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(e));
 
 	return (0);
