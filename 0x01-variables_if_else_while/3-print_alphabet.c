0268e7efcf1fcf21951542fb73e56588ce7f0208
Leac1m <obemichael10@gmail.com>
2022-12-11 15:23:41 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A script that prints all the alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while(ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
