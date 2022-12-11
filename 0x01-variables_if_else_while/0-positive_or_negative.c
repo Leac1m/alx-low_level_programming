367fe0d84a5c6ea310a02dad9cb63534445fb140
Leac1m <obemichael10@gmail.com>
2022-12-11 12:48:36 -0800

    A script that checks and print int unary

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 
+/**
+ * main - checks and print int urary
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -13,14 +16,15 @@ int main(void)
 	/* your code goes there */
 	if (n >= 0)
 	{
-		printf("%d is positive.", n);
-	} else if (n == 0)
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
 	{
-		printf("%d is Zero", n);
-	} else
+		printf("%d is zero", n);
+	}
+	else
 	{
 		printf("%d is negative", n);
 	}
-
-		return (0);
+	return (0);
 }
