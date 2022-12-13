cdc4399f1794becb00b8b6164cfd6525a9c20e35
Leac1m <obemichael10@gmail.com>
2022-12-12 19:52:29 -0800

    Multiple entry

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints all posible combination of three different digits,
+ * in ascending order, sepereted by a comma followed by a space.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num_1,  num_2,  num_3;
+
+	for (num_1 = 0; num_1 < 8; num_1++)
+	{
+		for (num_2 = num_1 + 1; num_2 < 9; num_2++)
+		{
+			for (num_3 = num_2 + 1; num_3 < 10; num_3++)
+			{
+				putchar((num_1 % 10) + '0');
+				putchar((num_2 % 10) + '0');
+				putchar((num_3 % 10) + '0');
+
+				if (num_1 == 7 && num_2 == 8 && num_3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
