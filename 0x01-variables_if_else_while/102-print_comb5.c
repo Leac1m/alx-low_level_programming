aff39643a77d6604dbedeb803349a43c6c8f5743
Leac1m <obemichael10@gmail.com>
2022-12-12 20:37:21 -0800

    Multiple

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,21 @@
  */
 int main(void)
 {
-	int num_1, num_2, num_3, num_4;
+	int num_1, num_2;
 
-	for (num_1 = 0; num_1 < 7; num_1++)
+	for (num_1 = 0; num_1 < 100; num_1++)
 	{
-		for (num_2 = num_1 + 1; num_2 < 8; num_2++)
+		for (num_2 = 0; num_2 < 100; num_2++)
 		{
-			for (num_3 = num_2 + 1; num_3 < 9; num_3++)
-			{
-				for (num_4 = num_3 + 1; num_4 < 10; num_4++)
-				{
-					putchar((num_1 % 10) + '0');
-					putchar((num_2 % 10) + '0');
-					putchar(' ');
-					putchar((num_3 % 10) + '0');
-					putchar((num_4 % 10) + '0');
-					if (num_1 == 6 && num_2 == 7 && num_3 == 8 && num_4 == 9)
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((num_1 / 10) + 48);
+			putchar((num_2 % 10) + 48);
+			putchar(' ');
+			putchar((num_2 / 10) + 48);
+			putchar((num_1 % 10) + 48);
+			if (num_1 == 98 || num_2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
