1eb1754695a52f9957207a79c694baea52a742d7
Leac1m <obemichael10@gmail.com>
2022-12-16 14:56:57 -0800

    AI pushing

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,10 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	int n = 0;
+
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 	if (month >= 2 && day >= 60)
 	{
+		n = 1;
 		day++;
 	}
 
@@ -31,7 +34,14 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 	printf("Day of the year: %d\n", day);
+	if (n == 0)
+	{
 	printf("Remaining days: %d\n", 365 - day);
+	}
+	else
+	{
+	printf("Remaining days: %d\n", 365 - day);
+	}
 	}
 	}
 }
