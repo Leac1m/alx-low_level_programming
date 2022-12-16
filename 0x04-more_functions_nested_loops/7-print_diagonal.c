f53cf5dd4e1dc1e02882a0072be334177ee7814c
Leac1m <obemichael10@gmail.com>
2022-12-16 13:48:54 -0800

    AI pushing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,7 +5,7 @@
 */
 void print_diagonal(int n)
 {
-	if (n != 0)
+	if (n > 0)
 	{
 	int r, c;
 
@@ -17,7 +17,7 @@ void print_diagonal(int n)
 		}
 		_putchar(92);
 		_putchar('\n');
-	
+
 	}
 	}
 	else
