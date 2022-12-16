c0471d3ac7ed83ad1cc5a2643bbeedb5ded4129c
Leac1m <obemichael10@gmail.com>
2022-12-16 15:09:23 -0800

    AI pushing

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,4 +8,4 @@ void positive_or_negative(int i);
 #endif /* MAIN_H */
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
-convert_day(month, day);
+int convert_day(int month, int day);
