6320bf06c863972be370fdc562fb796ddeaf8820
Leac1m <obemichael10@gmail.com>
2022-12-20 18:46:00 -0800

    incomplete

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+int i;
+i = 9;
+return (i);
+}
