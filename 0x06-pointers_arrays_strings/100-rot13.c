432cfa23e432cdbb12ae828837bc2015673a90f1
Leac1m <obemichael10@gmail.com>
2022-12-22 18:35:26 -0800

    on comment

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * rot13 -  function that encodes a string using rot13.
+ * @str: string
+ * Return: (str)
+ */
+char *rot13(char *str)
+{
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (*str != '\0')
+	{
+	if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z'))
+		{
+                if ((*str >= 'n' && *str <= 'z') || (*str >= 'N' && *str 'Z'))
+		{
+			*str -= 13;
+		}
+		else
+		{
+			*str += 13;
+		}
+		}
+		str++;
+	}
+	return (str);
+}
