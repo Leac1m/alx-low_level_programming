625f2e1a95b1118ff65a7c402d4002d772f47443
Leac1m <obemichael10@gmail.com>
2023-01-05 01:54:11 +0100

    multiple entry

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _pow_recursion - returns the value of x raised to the power of y.
  * @x: base
  * @y: power
- * Return: result
  */
 int _pow_recursion(int x, int y)
 {
@@ -13,4 +12,28 @@ int _pow_recursion(int x, int y)
 		return (-1);
 
 	return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _pow_recursion(1, 10);
+    printf("%d\n", r);
+    r = _pow_recursion(1024, 0);
+    printf("%d\n", r);
+    r = _pow_recursion(2, 16);
+    printf("%d\n", r);
+    r = _pow_recursion(5, 2);
+    printf("%d\n", r);
+    r = _pow_recursion(5, -2);
+    printf("%d\n", r);
+    r = _pow_recursion(-5, 3);
+    printf("%d\n", r);
+    return (0);
 }
