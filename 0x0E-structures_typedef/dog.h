6a07e0fda5409ae19971928e34610a7252e045fa
Leac1m <obemichael10@gmail.com>
2023-01-17 16:43:15 +0100

    type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Short description
+ * @name:name input
+ * @age: age input
+ * @owner: owner input
+ *
+ * Description: Longer description
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+/**
+ * init_dog - initialize a variable
+ * of type struct dog.
+ * @d: struct name
+ * @name:name input
+ * @age: age input
+ * @owner: owner input
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
 #endif
