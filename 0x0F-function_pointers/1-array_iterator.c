fe5e9ccb0a5feeeefc69072966890f9dfbedf6b0
Leac1m <obemichael10@gmail.com>
2023-01-18 21:22:01 +0100

    fucntion that ewcutes other fuction

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+/**
+ * array_iterator - function that executes other functions.
+ * @array: list of int(s).
+ * @size: size of array.
+ * @action: pointer to fuction
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
