5bf4ffe959d5ecc0b806d75390c35585c092353a
Leac1m <obemichael10@gmail.com>
2023-01-21 21:40:19 +0100

    unction that prints strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,15 +9,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list str;
 	unsigned int i;
+	char *s;
+
+	if (separator)
+	{
 
 	va_start(str, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(str, char *));
+		s = va_arg(str, char *);
+		printf("%s", (s) ? (s) : ("nil"));
 		if (i < (n + 1))
 			printf("%s",separator);
 	}
 	printf("\n");
 	va_end(str);
+	}
 }
