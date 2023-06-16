a5f39cb99cf031445c254203139bba96d730e517
Leac1m <obemichael10@gmail.com>
2023-06-16 22:07:24 +0100

    organized code according to betty requirements.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,30 +2,54 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int multiply(int num1, int num2) {
-	return num1 * num2;
+/**
+ * multiply - multiplies two numbers.
+ * @num1: First number.
+ * @num2: Second number.
+ * Return: product.
+ */
+int multiply(int num1, int num2)
+{
+	int mul = (num1 * num2);
+	return (mul);
 }
-
-int validateInput(char* str) {
+/**
+ * validateInput - validates if input is digit.
+ * @str: input.
+ * Return: 1 otherwise 0.
+ */
+int validateInput(char *str)
+{
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++) {
-		if (!isdigit(str[i])) {
-			return 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
 		}
 	}
-	return 1;
+	return (1);
 }
 
-int main(int argc, char* argv[]) {
+/**
+ * main - receives two digits as arguments, and print product.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0 (success) else 1.
+ */
+int main(int argc, char *argv[])
+{
 	int num1, num2, result;
 
-	if (argc != 3) {
+	if (argc != 3)
+	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (!validateInput(argv[1]) || !validateInput(argv[2])) {
+	if (!validateInput(argv[1]) || !validateInput(argv[2]))
+	{
 		printf("Error\n");
 		exit(98);
 	}
@@ -35,5 +59,5 @@ int main(int argc, char* argv[]) {
 
 	result = multiply(num1, num2);
 	printf("%d\n", result);
-	return 0;
+	return (0);
 }
